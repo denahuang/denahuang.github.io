092f33cb10dc961e5b895de2e0e4dcc910559e1a
denahuang <denahuangg@gmail.com>
2020-08-10 14:57:17 -0400

    included ScrollMagic

--- js/script.js
+++ js/script.js
@@ -6,3 +6,41 @@ $(document).ready(function () {
     });
 });
 
+// paper plane animation
+
+const flightPath = {
+    curviness: 1.25,
+    autoRotate: true,
+    values: [
+        {x: 100, y: -20},
+        {x: 300, y: 40},
+        {x: 500, y: 100},
+        {x: 750, y: -100},
+        {x: 400, y: -50},
+        {x: 600, y: 50},
+        {x: 800, y: 0},
+        {x: window.innerWidth, y: -250}
+    ]
+}
+
+const tween = new TimelineLite();
+
+tween.add (
+    TweenLite.to('.paper-plane', 1, {
+        bezier: flightPath,
+        ease: Power1.easeInOut
+    })
+);
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 1000, // 1000 = 1 second
+    triggerHook: 0
+
+})
+    .setTween(tween)
+    .addIndicators()
+    .setPin('.animation')
+    .addTo(controller);
