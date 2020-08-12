8726114b8db82b3f8ee29d150116a08804021ed8
denahuang <denahuangg@gmail.com>
2020-08-11 21:25:40 -0400

    updated path of paper plane

--- js/script.js
+++ js/script.js
@@ -9,17 +9,33 @@ $(document).ready(function () {
 // paper plane animation
 
 const flightPath = {
-    curviness: 1.25,
+    curviness: 1.5,
     autoRotate: true,
     values: [
-        {x: 100, y: -20},
-        {x: 300, y: 40},
-        {x: 500, y: 100},
-        {x: 750, y: -100},
-        {x: 400, y: -50},
-        {x: 600, y: 50},
-        {x: 800, y: 0},
-        {x: window.innerWidth, y: -250}
+        {x: 100, y: 0},
+        {x: -100, y: 0},
+        {x: 100, y: -250},
+        {x: 100, y: 0},
+
+
+        // {x: window.innerWidth/5, y: 0},                         // positive y: down
+        // {x: window.innerWidth/2, y: 20},                        // negative y: up
+        // {x: window.innerWidth-window.innerWidth/4, y: 100},
+        // {x: window.innerWidth/4, y: 50},
+        // {x: window.innerWidth-window.innerWidth/3, y: 0},
+        // {x: window.innerWidth-window.innerWidth/4, y: window.innerHeight/2},
+        // {x: window.innerWidth-window.innerWidth/4, y: window.innerHeight/2},
+        // {x: window.innerWidth/2, y: window.innerHeight-window.innerHeight/3},
+        // {x: -100, y: window.innerWidth/2}
+
+        // {x: 100, y: -20},
+        // {x: 300, y: 40},
+        // {x: 500, y: 100},
+        // {x: 750, y: -100},
+        // {x: 400, y: -50},
+        // {x: 600, y: 50},
+        // {x: 800, y: 0},
+        // {x: window.innerWidth, y: -250}
     ]
 }
 
@@ -35,12 +51,12 @@ tween.add (
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: '.animation',
-    duration: 1000, // 1000 = 1 second
-    triggerHook: 0
+    triggerElement: '.animation', 
+    duration: 500,     // 1000 = 1 second
+    triggerHook: 0    // 0: top, 1:bottom
 
 })
     .setTween(tween)
     .addIndicators()
-    .setPin('.animation')
+    .setPin('.animation')       // stays in area until animation is over
     .addTo(controller);
